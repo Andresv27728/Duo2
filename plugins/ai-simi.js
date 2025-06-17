601cbafe92b46baff7a11252a74ab3283331bbcf
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2025-06-17 09:46:36 -0500

    Add files via upload

--- plugins/ai-simi.js
+++ plugins/ai-simi.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch';
 
 const handler = async (m, {conn, text, command, args, usedPrefix}) => {
 
-if (!text) conn.reply(m.chat, `${emoji} Te faltó el texto para hablar con la *Bot*`, m, rcanal);
+if (!text) conn.reply(m.chat, `${emoji} Te faltó el texto para hablar con la *Bot*`, m);
 try {
 // await m.react(emojis)
 const resSimi = await simitalk(text);
@@ -17,7 +17,7 @@ handler.help = ['simi', 'bot'];
 handler.tags = ['fun'];
 handler.group = true;
 handler.register = true
-handler.command = ['bot', 'nino', 'nino']
+handler.command = ['yuki', 'Yuki']
 
 export default handler;
 
